9af464ecddaff0ce63564442aac04b9d4b3aa900
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-12-13 12:04:02 -0600

    got test 8

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,29 +1,34 @@
 class LinkedList
 
-  def initialize
+  def initialize(*payload)
     @count = 0
   end
 
-  def add_item(item)
-    new_item = LinkedListItem.new(item)
+  def add_item(payload)
+    new_item = LinkedListItem.new(payload)
     item = @head
       if @head.nil?
         @head = new_item
       else
-       @count.times do
-       item = item.next_list_item
-       end
-      item.next_list_item = new_item
-      @count += 1
+        @count.times do
+        item = item.next_list_item
+        end
+        #
+        # else
+          item.next_list_item = new_item
+          @count += 1
+        # end
       end
   end
 
   def get(index)
+    raise IndexError if index < 0
     item = @head
     if index == 0
       @head.payload
     else
       index.times do
+        raise IndexError if item == nil
         item = item.next_list_item
       end
       item.payload
